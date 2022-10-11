3ad7aa5bdfce23d2b0c05b3aeecb23232ecdc543
Alexsis <aleksei3833@gmail.com>
2022-10-12 02:34:40 +0400

    Проработка пользовательского интерфейса и таблиц БД

---
+++ DBWorking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace EnterpriseNCC1701X
+{
+   public class DBWorking
+    {
+        SqlConnectionStringBuilder strCon = new SqlConnectionStringBuilder()
+        {
+            DataSource = @" (localdb)\MSSQLLocalDb",
+            InitialCatalog = "NCC1701XDB",
+            IntegratedSecurity = true,
+            Pooling = false
+        };
+
+        
+       
+
+
+    }
+} 
